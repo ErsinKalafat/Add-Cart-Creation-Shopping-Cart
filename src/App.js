9f0c95c7c4268d43c636a8a7689d79de189fd9b4
ErsinKalafat <ersin0105@gmail.com>
2019-07-31 03:40:13 +0300

    Proje Yüklendi. Mevcut sürümde ürün listesi grid şeklinde listelendi. Gerekli tasarımlar ve ayarlamalar yapıldı. Cart.js oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,176 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Tasarim from './tasarim.module.scss';
+import Cart from "./Cart.js";
+import urun1 from './images/urun1.png';
+import urun2 from './images/urun2.png';
+import urun3 from './images/urun3.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <input className={Tasarim.mycartbuton} type="button" value="My Cart"/>
+                        <div className={Tasarim.ersinkalafat} >Ersin Kalafat Hürriyet Emlak Case Study - 0506 537 28 49</div>
+                    </div>
+                </Router>
+
+
+                <hr/>
+                <div className={Tasarim.urunlistesi}>
+                    <div className={Tasarim.urun}>
+                        <table className={Tasarim.urundizayn}>
+                            <tr>
+                                <td>
+                                    <img src={urun1}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={Tasarim.urunaciklamasi}>
+                                    Chevron Tempered Glass Guard for Lenovo K6 Power
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    730 TL
+                                </td>
+                                <td>
+                                    <input className={Tasarim.buton} type="submit" value="ADD BASKET"/>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
+
+                    <div className={Tasarim.urun}>
+                        <table className={Tasarim.urundizayn}>
+                            <tr>
+                                <td>
+                                    <img src={urun2}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={Tasarim.urunaciklamasi}>
+                                    Kajal Sarees Printed Bhagalpuri Art Silk Sari
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    730 TL
+                                </td>
+                                <td>
+                                    <input className={Tasarim.buton} type="submit" value="ADD BASKET"/>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
+
+                    <div className={Tasarim.urun}>
+                        <table className={Tasarim.urundizayn}>
+                            <tr>
+                                <td>
+                                    <img src={urun3}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={Tasarim.urunaciklamasi}>
+                                    Indian Polity 5 Edition
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    730 TL
+                                </td>
+                                <td>
+                                    <input className={Tasarim.buton} type="submit" value="ADD BASKET"/>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
+                    <div className={Tasarim.urun}>
+                        <table className={Tasarim.urundizayn}>
+                            <tr>
+                                <td>
+                                    <img src={urun2}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={Tasarim.urunaciklamasi}>
+                                    Kajal Sarees Printed Bhagalpuri Art Silk Sari
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    730 TL
+                                </td>
+                                <td>
+                                    <input className={Tasarim.buton} type="submit" value="ADD BASKET"/>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
+                    <div className={Tasarim.urun}>
+                        <table className={Tasarim.urundizayn}>
+                            <tr>
+                                <td>
+                                    <img src={urun1}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={Tasarim.urunaciklamasi}>
+                                    Chevron Tempered Glass Guard for Lenovo K6 Power
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    730 TL
+                                </td>
+                                <td>
+                                    <input className={Tasarim.buton} type="submit" value="ADD BASKET"/>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
+                    <div className={Tasarim.urun}>
+                        <table className={Tasarim.urundizayn}>
+                            <tr>
+                                <td>
+                                    <img src={urun3}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={Tasarim.urunaciklamasi}>
+                                    Indian Polity 5 Edition
+                                </td>
+                            </tr>
+                            <tr className={Tasarim.altdizayn}>
+                                <td>
+                                    730 TL
+                                </td>
+                                <td>
+                                    <input className={Tasarim.buton} type="submit" value="ADD BASKET"/>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
+
+
+
---
+++ src/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tasarim from "./tasarim.module.scss";
+
+class Cart extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+
+
+
+            </div>
+
+        );
+    }
+
+}
+
+export default Cart;
